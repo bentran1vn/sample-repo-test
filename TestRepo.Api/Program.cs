1527747778a84d902796a176fa86e23cc187c3a9
bentran1vn <tan182205@gmail.com>
2026-03-21 17:45:22 +0700

    Sample repo

---
+++ TestRepo.Api/Program.cs
@@ -0,0 +1,34 @@
+using TestRepo.Api.Extensions;
+using TestRepo.Api.Middlewares;
+
+using JwtService = TetPee.Service.JwtService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddJwtServices(builder.Configuration);
+builder.Services.AddSwaggerServices();
+
+builder.Services.AddScoped<JwtService.IService, JwtService.Service>();
+
+var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerAPI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
